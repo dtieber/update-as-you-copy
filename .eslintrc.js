da3c71e2d8dc2950c8c680ddb7bc268c8e4eaeb7
Daniel Tieber <daniel.tieber@redbull.com>
2022-01-03 09:53:18 +0100

    build: allow modification for all objects starting with `clone`

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,7 +38,7 @@ module.exports = {
     'no-extra-parens': 'error',
     'no-multiple-empty-lines': ['error', { 'max': 1 }],
     'no-multi-spaces': 'error',
-    'no-param-reassign': ['error', { 'props': true }],
+    'no-param-reassign': ['error', { 'props': true, 'ignorePropertyModificationsForRegex': ['^clone'] }],
     'no-return-await': 'error',
     'no-shadow': 'off',
     'object-curly-spacing': ['error', 'always'],
